100a259ae5922d2277b2123a85ee841b852cc17c
63070055 <73680542+63070055@users.noreply.github.com>
2022-11-11 07:15:37 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '0996286167Earth',
+    database: 'projectmobile',
+})
+app.get('/bts', (req, res) => {
+    db.query("SELECT * FROM station_bts where id != 49", (err, result) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.json(result);
+        }
+    })
+});
+app.get('/mrt', (req, res) => {
+    db.query("SELECT * FROM station_mrt", (err, result) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.json(result);
+        }
+    })
+});
+app.get('/direcBts', (req, res) => {
+    db.query("SELECT * FROM station_bts", (err, result) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.json(result);
+        }
+    })
+});
+app.get('/travelBts', (req, res) => {
+    db.query("SELECT * FROM travel", (err, result) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.json(result);
+        }
+    })
+});
+app.listen(3000, () => {
+    console.log(`Example app listening at http://localhost:3000`)
+})
