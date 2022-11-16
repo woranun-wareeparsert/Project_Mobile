39c6ca2e9dc26d20cd1486213b4df6df162ef8eb
woranun-wareeparsert <73680983+woranun-wareeparsert@users.noreply.github.com>
2022-11-17 03:09:19 +0700

    Update TravelScreen.js

--- screens/TravelScreen.js
+++ screens/TravelScreen.js
@@ -9,33 +9,43 @@ import {
     Platform,
     Pressable,
     Modal,
-    Alert
+    Alert,
+    Image,
+    ScrollView,
+    SafeAreaView
 } from "react-native";
 import Axios from "axios";
 import { useState, useEffect } from "react";
 import CategoryGridTile from "../components/CategoryGridTile";
-
 
 const TravelScreen = ({ navigation, route }) => {
     const { prev, Endstation } = route.params;
     const [direcpass, setdirecpass] = React.useState([]);
     const [check, setCheck] = React.useState(0);
 
+    var IdImage = 0;
+    const [allimage, setallimage] = React.useState([]);
+
     //open-close Modal
     const [visible, setVisible] = React.useState(false);
-    const showModal = () => setVisible(true);
+    const showModal = (item) => {
+        IdImage = item.item.id;
+        setVisible(true);
+        let myloop1 = [];
+        Axios.get("http://localhost:3000/img_travel").then((res) => {
+            res.data.forEach(function (item1, index) {
+                if (item1.travel_id == IdImage) {
+                    myloop1.push(item1.image);
+                }
+            })
+            setallimage(myloop1);
+        });
+    }
     const hideModal = () => setVisible(false);
 
     const getBtsDirect = () => {
         Axios.get("http://localhost:3000/travelBts").then((res) => {
             let myloop = [];
-            // Allsta.forEach(function (item, index) {
-            //     res.data.forEach(function (item1, index) {
-            //         if (item1.idbts == item.id) {
-            //             myloop.push(item1);
-            //         }
-            //     })
-            // })
             res.data.forEach(function (item1, index) {
                 if (Endstation == item1.idbts) {
                     myloop.push(item1);
@@ -60,41 +70,79 @@ const TravelScreen = ({ navigation, route }) => {
                 style={styles.text}
                 title={item.item.tname}
                 color="#50586C"
-                onPress={showModal}
+                onPress={() => showModal(item)}
             />
         );
     };
 
     return (
-        <View style={styles.screen}>
-            <Modal animationType="fade" transparent={true} visible={visible} onDismiss={hideModal}>
-                <View style={styles.centeredView}>
-                    <View style={styles.modalView}>
-                        <Text style={styles.modalText}>Hello World!</Text>
-                        <Pressable
-                            style={[styles.button, styles.buttonClose]}
-                            onPress={hideModal}
-                        >
-                            <Text style={styles.textStyle}>Hide Modal</Text>
-                        </Pressable>
-                    </View>
-                </View>
-            </Modal>
+        <SafeAreaView style={styles.container}>
+            <ScrollView>
+                <View style={styles.screen}>
+                    <Modal animationType="fade" transparent={true} visible={visible} onDismiss={hideModal}>
+                        <View style={styles.centeredView}>
+                            <View style={styles.modalView}>
+                                <View style={styles.showimage}>
+                                    <ScrollView
+                                        horizontal={true}
+                                        style={styles.contentContainer}>
+                                        <FlatList
+                                            horizontal={true}
+                                            showsHorizontalScrollIndicator={false}
+                                            style={styles.task}
+                                            data={allimage}
+                                            renderItem={({ item, index }) => (
+                                                <Image
+                                                    horizontal={true}
+                                                    showsHorizontalScrollIndicator={false}
+                                                    source={require("../assets/" + item)}
+                                                    key={index}
+                                                    style={{
+                                                        width: 260,
+                                                        height: 300,
+                                                        // borderWidth: 2,
+                                                        resizeMode: 'cover',
+                                                        resizeMode: 'contain',
+                                                        margin: 8
+                                                    }}></Image>
+                                            )} />
+                                    </ScrollView>
+                                </View>
 
-            <View style={styles.screentop}>
-                <FlatList style={styles.task} data={direcpass} renderItem={renderCategories} numColumns={2} />
-            </View>
-            <Button
-                title="เส้นทางเพิ่มเติม"
-                color="#50586C"
-                onPress={() => {
-                    navigation.navigate("Detail");
-                }}>Next Page</Button>
-        </View>
+                                <Text style={styles.modalText}>Hello World!</Text>
+                                <Pressable
+                                    style={[styles.button, styles.buttonClose]}
+                                    onPress={hideModal}
+                                >
+                                    <Text style={styles.textStyle}>Hide Modal</Text>
+                                </Pressable>
+                            </View>
+
+                        </View>
+
+                    </Modal>
+
+                    <View style={styles.screentop}>
+                        <FlatList style={styles.task} data={direcpass} renderItem={renderCategories} numColumns={2} />
+                    </View>
+                    <Button
+                        title="เส้นทางเพิ่มเติม"
+                        color="#50586C"
+                        onPress={() => {
+                            navigation.navigate("Detail");
+                        }}>Next Page</Button>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
+    container: {
+        height: "100%",
+        flex: 1,
+        backgroundColor: "#DCE2F0",
+    },
     screen: {
         height: "100%",
         flex: 1,
@@ -113,6 +161,17 @@ const styles = StyleSheet.create({
         backgroundColor: "green",
         fontSize: 18,
     },
+    img: {
+        width: 50,
+        height: 50,
+    },
+    showimage: {
+        width: "100%",
+
+    },
+    task: {
+        padding: 10,
+    },
 
 
     centeredView: {
@@ -124,6 +183,7 @@ const styles = StyleSheet.create({
     },
     modalView: {
         margin: 20,
+        width: "85%",
         backgroundColor: "white",
         borderRadius: 20,
         padding: 35,
